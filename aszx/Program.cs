bc0760bd47b1edaccf93eb314738804ad86da26a
*jeyhun <*jeyhunag@code.edu.az*>
2022-05-17 15:25:23 +0300

    cdbhvf

---
+++ aszx/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace aszx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isContinue = true;
+            
+            while (isContinue)
+            {
+                Console.WriteLine("Yasinizi daxil edin");
+                int yas = Convert.ToInt32(Console.ReadLine());                
+                if (yas>18)
+                {
+                    isContinue = true;
+                    
+                }
+                if (yas<18)
+                {
+                    isContinue = false;
+                    Console.WriteLine("Sizin yasiniz azdi qeydiyyatdan kece bilmediniz");
+                }
+                Console.WriteLine("Balinizi daxil edin");
+                int bal = Convert.ToInt32(Console.ReadLine());
+                if (bal>50)
+                {
+                    isContinue = true;
+                    Console.WriteLine("Tebrik edirik qeydiyyatdan kecdiniz");
+                }
+                if (bal<50)
+                {
+                    isContinue = false;
+                    Console.WriteLine("Teesuf edirik baliniz yeterli deyil");
+                }
+            }
+                
+            
+        }
+    }
+}
